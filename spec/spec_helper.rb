7c681cfdc5cfe3d45db0f0f96ec5fd22ba750bd8
Tim Chambers <tim@possibilogy.com>
2017-06-29 15:27:28 -0700

    Capture coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,6 @@ ENV['DB'] ||= 'sqlite'
 
 require 'simplecov'
 
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
-end
-
 SimpleCov.start do
   add_filter '/spec/'
 end
