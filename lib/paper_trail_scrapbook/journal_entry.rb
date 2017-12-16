c2be0e25f3e0fec7ceb01e742821b57e25afcaf0
Jason Dinsmore <jason@dinjas.com>
2017-12-15 16:31:21 -0800

    Add journal entry class - equivalent to Chapter but for UserJournal

---
+++ lib/paper_trail_scrapbook/journal_entry.rb
@@ -0,0 +1,33 @@
+require_relative 'version_helpers'
+
+module PaperTrailScrapbook
+  # Class JournalEntry provides single version history analysis
+  #
+  # @author Jason Dinsmore <jason@dinjas.com>
+  #
+  class JournalEntry
+    include Concord.new(:version)
+    include Adamantium::Flat
+    include PaperTrailScrapbook::VersionHelpers
+
+    delegate :event, to: :version
+
+    # Single version historical analysis
+    #
+    # @return [String] Human readable description of changes
+    #
+    def story
+      updates = changes
+      return unless create? || updates.present? || !config.filter_non_changes
+
+      "#{preface}\n#{updates}"
+    end
+
+    private
+
+    def preface
+      "On #{whenn}, #{kind} #{model}[#{model_id}]:".squeeze(' ')
+    end
+
+  end
+end
