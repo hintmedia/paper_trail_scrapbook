7d60af3ebc5b4c132a715101a27b3900b4db516a
Jason Dinsmore <jason@dinjas.com>
2020-12-18 09:57:26 -0800

    Put class in module

--- lib/paper_trail_scrapbook/versions.rb
+++ lib/paper_trail_scrapbook/versions.rb
@@ -1,26 +1,29 @@
 # frozen_string_literal: true
 
-class Versions
-  include Concord.new(:object)
+module PaperTrailScrapbook
+  class Versions
+    include Concord.new(:object)
 
-  def versions
-    filtered(object.versions | related_content).sort_by(&:created_at)
-  end
-
-  def related_content
-    if object.respond_to?(:trailed_related_content)
-      object.trailed_related_content.compact.flat_map(&:versions)
-    else
-      []
+    def versions
+      filtered(object.versions | related_content).sort_by(&:created_at)
     end
-  end
 
-  def filtered(object_versions)
-    if object.respond_to?(:version_filter)
-      object_versions.select { |v| object.version_filter(v) }
-    else
-      object_versions
+    def related_content
+      if object.respond_to?(:trailed_related_content)
+        object.trailed_related_content.compact.flat_map(&:versions)
+      else
+        []
+      end
     end
+
+    def filtered(object_versions)
+      if object.respond_to?(:version_filter)
+        object_versions.select { |v| object.version_filter(v) }
+      else
+        object_versions
+      end
+    end
+
   end
 
 end
