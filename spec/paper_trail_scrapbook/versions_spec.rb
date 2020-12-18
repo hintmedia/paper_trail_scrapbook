97066ef8799d18cea8c8c903c84ec4cb3a2bc636
Jason Dinsmore <jason@dinjas.com>
2020-12-18 11:10:38 -0800

    Strengthen spec

--- spec/paper_trail_scrapbook/versions_spec.rb
+++ spec/paper_trail_scrapbook/versions_spec.rb
@@ -5,61 +5,96 @@ require 'paper_trail_scrapbook/versions'
 
 module PaperTrailScrapbook
   ::RSpec.describe Versions do
-
     before do
       PaperTrailScrapbook.config.whodunnit_class = Person
       PaperTrail.request.whodunnit = person.id
     end
-
-    let(:person) { Person.create(name: 'The Tim Man') }
 
     let(:book) do
       b = Book.new(title: 'How the Grinch stole Xmas')
       b.save!
       b
     end
-
-    let(:author) do
-      p = Person.new(name: 'Dr. Seuss')
-      p.save!
-      p
-    end
-
-    let(:authorship) do
-      a = Authorship.new(book: book, author: author)
+    let(:person) { Person.create(name: 'The Tim Man') }
+    let!(:authorship) do
+      a = Authorship.new(book: book, author: person)
       a.save!
       a
     end
 
-    let(:object) { described_class.new(authorship) }
+    let(:object) { described_class.new(book) }
 
-    # TODO: auto-generated
     describe '#versions' do
       it 'versions' do
+        book.update!(title: 'New Title')
+
         result = object.versions
 
-        expect(result).not_to be_nil
+        expect(result).to eql(book.versions.to_a)
+        expect(result.size).to equal(2)
       end
     end
 
     # TODO: auto-generated
     describe '#related_content' do
-      it 'related_content' do
+      it 'related_content returns nothing if no related content' do
         result = object.related_content
 
-        expect(result).not_to be_nil
+        expect(result).to eql([])
+      end
+
+      it 'returns related content' do
+        allow(book).to receive(:respond_to?)
+          .with(:trailed_related_content).and_return(true)
+        allow(book).to receive(:respond_to?)
+          .with(:trailed_related_content, true).and_return(true)
+        allow(book).to receive(:trailed_related_content) do
+          book.authorships
+        end
+
+        result = object.related_content
+
+        expect(result).to eql(authorship.versions.to_a)
       end
     end
 
-    # TODO: auto-generated
     describe '#filtered' do
-      it 'filtered' do
-        result = object.filtered(authorship.versions)
+      it 'returns versions if no filter' do
+        result = object.filtered(book.versions.to_a)
 
-        expect(result).not_to be_nil
+        expect(result).to eql(book.versions.to_a)
+      end
+
+      it 'returns filtered versions if filter' do
+        allow(book).to receive(:respond_to?)
+          .with(:version_filter, true).and_return(true)
+        allow(book).to receive(:respond_to?)
+          .with(:version_filter).and_return(true)
+        allow(book).to receive(:version_filter) do |_version|
+          nil
+        end
+
+        result = object.filtered(book.versions.to_a)
+
+        expect(result).to eql([])
+      end
+
+      it 'returns filtered versions if filter' do
+        book.update!(title: 'New Title')
+
+        allow(book).to receive(:respond_to?).with(:version_filter, true).and_return(true)
+        allow(book).to receive(:respond_to?).with(:version_filter).and_return(true)
+        allow(book).to receive(:version_filter) do |version|
+          if version.event.eql?('create')
+            version
+          end
+        end
+
+        result = object.filtered(book.versions)
+
+        expect(result).to eql([book.versions.first])
+        expect(result.size).not_to eql(book.versions.size)
       end
     end
-
   end
-
 end
