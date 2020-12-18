64c436f367be7b61d68cc93e5c92fb7876674a41
Jason Dinsmore <jason@dinjas.com>
2020-12-18 07:55:10 -0800

    Extract separate methods

--- lib/paper_trail_scrapbook/versions.rb
+++ lib/paper_trail_scrapbook/versions.rb
@@ -4,19 +4,27 @@ class Versions
   include Concord.new(:object)
 
   def versions
-    versions = object.versions
+    sorted(filtered(with_related(object.versions)))
+  end
 
-    if object.respond_to?(:trailed_related_content)
-      object.trailed_related_content.compact.each do |trc|
-        versions |= trc.versions
-      end
+  def with_related(object_versions)
+    unless object.respond_to?(:trailed_related_content)
+      return object_versions
     end
 
-    if object.respond_to?(:version_filter)
-      versions.select! { |v| object.version_filter(v) }
+    object_versions | object.trailed_related_content.compact.map(&:versions)
+  end
+
+  def filtered(object_versions)
+    unless object.respond_to?(:version_filter)
+      return object_versions
     end
 
-    versions.sort_by(&:created_at
+    object_versions.select { |v| object.version_filter(v) }
+  end
+
+  def sorted(object_versions)
+    object_versions.sort_by(&:created_at)
   end
 
 end
